8263b109bd18c45db787eb2e878ca014a46ea55a
suchetanrs <suchetan.saravanan@gmail.com>
2026-04-25 11:31:21 +0530

    Add button to capture the scene

--- gsplat_rviz_plugin/include/gsplat_rviz_plugin/displays/gsplat_display.hpp
+++ gsplat_rviz_plugin/include/gsplat_rviz_plugin/displays/gsplat_display.hpp
@@ -15,6 +15,7 @@
 #include "rviz_common/properties/int_property.hpp"
 #include "rviz_common/properties/property.hpp"
 #include "rviz_common/properties/ros_topic_property.hpp"
+#include "rviz_common/properties/string_property.hpp"
 #include "rviz_common/properties/vector_property.hpp"
 #include "gsplat_rviz_plugin/splat_loaders/i_splat_source.hpp"
 #include "gsplat_rviz_plugin/visibility_control.hpp"
@@ -55,6 +56,7 @@ private Q_SLOTS:
   void onClipChanged();
   void onTransparencyModeChanged();
   void onWboitTuningChanged();
+  void onCaptureTriggerChanged();
 
 private:
   enum class SourceKind { None, File, Topic };
@@ -64,6 +66,7 @@ private:
   void installSource(std::unique_ptr<ISplatSource> source, SourceKind kind);
   void onLoadResult(LoadResult result, SourceKind kind, uint64_t gen);
   SourceMode currentMode() const;
+  void captureScreenshot(const std::string & path);
 
   // Push transparency_mode_property_ → SplatCloud::setOitEnabled and
   // attach/detach the WBOIT compositor on the current viewport. Called
@@ -107,6 +110,10 @@ private:
   rviz_common::properties::FloatProperty *      wboit_weight_scale_property_;
   rviz_common::properties::FloatProperty *      wboit_weight_exponent_property_;
   rviz_common::properties::FloatProperty *      wboit_alpha_discard_property_;
+
+  // Under "Advanced / Scene Capture" sub-group.
+  rviz_common::properties::StringProperty *     capture_path_property_;
+  rviz_common::properties::BoolProperty *       capture_trigger_property_;
 
   std::unique_ptr<SplatCloud>   splat_cloud_;
   std::unique_ptr<ISplatSource> source_;
